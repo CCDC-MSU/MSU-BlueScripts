44b2017e33e23e7e47f637b64bab12be91fb0d3f
antimony051 <sb3771+github@msstate.edu>
2025-12-28 01:15:02 -0500

    removed old useless modules

--- Linux/fabric_deploy/utilities/modules/__init__.py
+++ Linux/fabric_deploy/utilities/modules/__init__.py
@@ -5,13 +5,11 @@ Hardening modules for CCDC framework
 from .base import HardeningModule, HardeningCommand, HardeningResult, HardeningAction, CommandAction, PythonAction
 from .agent_account import AgentAccountModule
 from .package_installer import PackageInstallerModule
-from .installation_status import InstallationStatusModule
 from .logging_setup import LoggingSetupModule
 from .ssh_hardening import SSHHardeningModule
 from .kernel_hardening import KernelHardeningModule
 from .firewall_hardening import FirewallHardeningModule
 from .service_hardening import ServiceHardeningModule
-from .file_permissions import FilePermissionsModule
 from .bash_scripts import BashScriptHardeningModule
 from .user_hardening import UserHardeningModule
 
@@ -24,13 +22,11 @@ __all__ = [
     'PythonAction',
     'AgentAccountModule',
     'PackageInstallerModule',
-    'InstallationStatusModule',
     'LoggingSetupModule',
     'SSHHardeningModule',
     'KernelHardeningModule', 
     'FirewallHardeningModule',
     'ServiceHardeningModule',
-    'FilePermissionsModule',
     'BashScriptHardeningModule',
     'UserHardeningModule',
 ]
